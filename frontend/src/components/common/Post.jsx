db827c13a5317730603914a330c39960f7a946bc
Prajwal <prajwal77dwivedi@gmail.com>
2025-07-10 19:16:03 +0530

    Delete Post Functionality Added

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -5,19 +5,54 @@ import { FaRegBookmark } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+import LoadingSpinner from "./LoadingSpinner";
 
 const Post = ({ post }) => {
   const [comment, setComment] = useState("");
+  const { data: authUser } = useQuery({ queryKey: ["authUser"] }); //we're using this to check who is the current user
+  const queryClient = useQueryClient(); //we're using this to invalidate the posts query to refetch the posts
+
+  //below is the mutation function to delete the post, it will hit the backend and delete the post from the database
+  const { mutate: deletePost, isPending: isDeletingPost } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch(`/api/posts/${post._id}`, {
+          //actual link to the backend route
+          method: "DELETE",
+          credentials: "include",
+        });
+        const data = await res.json(); //getting the response from the backend
+        if (!res.ok) {
+          throw new Error(data.error || "Failed to delete post");
+        }
+        return data;
+      } catch (error) {
+        console.log(error);
+        throw new Error(error.message);
+      }
+    },
+    onSuccess: () => {
+      toast.success("Post deleted successfully");
+      queryClient.invalidateQueries({ queryKey: ["posts"] }); //invalidating the posts query to refetch the posts
+    },
+  });
   const postOwner = post.user;
   const isLiked = false;
 
-  const isMyPost = true;
+  const isMyPost = authUser?._id === post.user._id; //checking if the current user is the owner of the post
 
   const formattedDate = "1h";
 
   const isCommenting = false;
 
-  const handleDeletePost = () => {};
+  //delete post function to delete the post
+  const handleDeletePost = () => {
+    if (isMyPost) {
+      deletePost();
+    }
+  };
 
   const handlePostComment = (e) => {
     e.preventDefault();
@@ -50,10 +85,13 @@ const Post = ({ post }) => {
             </span>
             {isMyPost && (
               <span className="flex justify-end flex-1">
-                <FaTrash
-                  className="cursor-pointer hover:text-red-500"
-                  onClick={handleDeletePost}
-                />
+                {!isDeletingPost && (
+                  <FaTrash
+                    className="cursor-pointer hover:text-red-500"
+                    onClick={handleDeletePost}
+                  />
+                )}
+                {isDeletingPost && <LoadingSpinner size={"sm"} />}
               </span>
             )}
           </div>
